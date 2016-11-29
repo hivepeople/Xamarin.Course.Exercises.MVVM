9295312f893fdf29260d2299bf5c150ad0db6f96
bvi@hivepeople.dk <snb@hivepeople.dk>
2016-11-29 12:12:07 +0100

    Added a simple loginpage

---
+++ SimpleLoginExample/SimpleLoginExample/SimpleLoginExample/SimpleLoginPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace SimpleLoginExample
+{
+    public partial class SimpleLoginPage : ContentPage
+    {
+        private SimpleLoginViewModel simpleViewModel;
+        
+        public SimpleLoginPage(SimpleLoginViewModel simpleViewModel)
+        {
+
+            InitializeComponent();
+            this.simpleViewModel = simpleViewModel;
+            BindingContext = this.simpleViewModel;
+
+        }
+    }
+}
---
+++ SimpleLoginExample/SimpleLoginExample/SimpleLoginExample/SimpleLoginViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLoginExample
+{
+    public class SimpleLoginViewModel : INotifyPropertyChanged
+    {
+        private string versionText = "1.2.4";
+        private string code;
+        private bool entryCodeOk;
+        private string statusText = "Indtast telefonnummer";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string VersionText
+        {
+            get
+            {
+                return versionText;
+            }
+
+            set
+            {
+                versionText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Code
+        {
+            get
+            {
+                return code;
+            }
+
+            set
+            {
+                code = value;
+                NotifyPropertyChanged();
+                if (Code!=null && Code.Length == 8)
+                {
+                    EntryCodeIsOk = true;
+                }
+                else
+                {
+                    EntryCodeIsOk = false;
+                }
+            }
+        }
+
+        public bool EntryCodeIsOk
+        {
+            get
+            {
+                return entryCodeOk;
+            }
+
+            set
+            {
+                entryCodeOk = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string StatusText
+        {
+            get
+            {
+                return statusText;
+            }
+
+            set
+            {
+                statusText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public virtual void NotifyPropertyChanged([CallerMemberName] string propertyName="")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+    }
+}
